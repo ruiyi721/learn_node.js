c317a7c83a2414984d68367093f04e53710cc4f9
Dora <ohya721@gmail.com>
2022-12-05 18:27:57 +0800

    1205

--- src/routes/member.js
+++ src/routes/member.js
@@ -6,4 +6,28 @@ router.get('/login', (req, res) => {
     res.render('member/login');
 });
 
+router.post('/login', (req, res) => {
+    const users = {
+        'dora': {
+            nickname: '朵拉',
+            pw: '1234'
+        },
+        'sam': {
+            nickname: '山姆',
+            pw: '5678'
+        },
+    };
+
+    if(req.body.account && users[req.body.account]) {
+        // 帳號是對的
+        if(req.body.password === users[req.body.account].pw) {
+            // 密碼也是對的
+        }
+    }
+
+    res.json({
+        body: req.body
+    })
+});
+
 module.exports = router;
